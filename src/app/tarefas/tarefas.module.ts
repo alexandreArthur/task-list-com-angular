b84d6a83f12464960478eaf290a129779f9b9e33
Arthur Alexandre <arthur_dda@hotmail.com>
2021-08-27 14:06:52 -0300

    directive para tarefa concluída

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import{ RouterModule } from '@angular/router';
 import{ FormsModule } from '@angular/forms';
 
-import { TarefaService } from './shared';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
-import { EditarTarefaComponent } from './editar'
+import { EditarTarefaComponent } from './editar';
 
 
 
@@ -14,7 +14,8 @@ import { EditarTarefaComponent } from './editar'
   declarations: [
     ListarTarefaComponent,
     CadastrarTarefaComponent,
-    EditarTarefaComponent
+    EditarTarefaComponent,
+    TarefaConcluidaDirective
   ],
   imports: [
     CommonModule, RouterModule, FormsModule
